df3b3d1ff14857d66701d00b4fd4ba3f548d5e0e
Christopher Palazzo <23Palazzoc@wiufsd.com>
2022-03-21 07:59:49 -0400

    worked on cube generation for arm

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import cv2
 from collections import deque
-from scipy.linalg import qr, svd
 import pyqtgraph.opengl as gl
 from pyqtgraph.Qt import QtGui
-
 
 # Numbers that work well (in inches) with the graph and keep proper proportions
 xtable = 108
@@ -20,16 +18,46 @@ ynet = 6  # Kinda like a z coord after 90 degree rotation
 
 # Init robot arm in frame
 def init_arm(w):
-
     # -- Create Arm Base --
-    basePlate = 12
+    basePlate = 12  # in inches
     armBasePlate = gl.GLGridItem()  # Create base
     armBasePlate.setColor((255, 0, 255, 255))
     armBasePlate.translate(-10.5 - (basePlate / 2), (ytable / 2), 0)  # Move to correct coord
     armBasePlate.setSize(basePlate, basePlate)  # Size table
     w.addItem(armBasePlate)
 
-    return armBasePlate
+    # -- Create Base Stands --
+    baseStandVerts = np.array([[1, 0, 0],  # 0
+                                [0, 0, 0],  # 1
+                                [0, 1, 0],  # 2
+                                [0, 0, 1],  # 3
+                                [1, 1, 0],  # 4
+                                [1, 1, 1],  # 5
+                                [0, 1, 1],  # 6
+                                [1, 0, 1]])  # 7
+
+    baseStandFaces = np.array([[1, 0, 7], [1, 3, 7],
+                                [1, 2, 4], [1, 0, 4],
+                                [1, 2, 6], [1, 3, 6],
+                                [0, 4, 5], [0, 7, 5],
+                                [2, 4, 5], [2, 6, 5],
+                                [3, 6, 5], [3, 7, 5]])
+
+    colors = np.array([[1, 0, 0, 1] for i in range(12)])
+
+    # Create same stands
+    baseStand1 = gl.GLMeshItem(vertexes=baseStandVerts, faces=baseStandFaces, faceColors=colors,
+                               drawEdges=True, edgeColor=(0, 0, 0, 1))
+    baseStand2 = gl.GLMeshItem(vertexes=baseStandVerts, faces=baseStandFaces, faceColors=colors,
+                               drawEdges=True, edgeColor=(0, 0, 0, 1))
+
+    # Translate stands
+
+    # Add to frame
+    w.addItem(baseStand1)
+    w.addItem(baseStand2)
+
+    return baseStand1
 
 
 # Main Testing Loop
@@ -64,7 +92,6 @@ def main():
     while True:
 
         # update arm
-
 
         # detect keys
         key = cv2.waitKey(1) & 0xFF
