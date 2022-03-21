c2d0078e566c50fb55b390db2508bf5d68fbd3f2
Chris Palazzo <61244661+ChrisPal323@users.noreply.github.com>
2022-03-21 16:20:14 -0400

    cont working on arm render

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ def init_arm(w):
                             [2, 4, 5], [2, 6, 5],
                             [3, 6, 5], [3, 7, 5]])
 
-    # Render colors for all
-    renderColors = np.array([[0.5, 0.3, 0.2, 1] for i in range(12)])
-
     # --- Create Arm Base ---
     basePlateLength = 12  # in inches
     baseDistFromTable = -10.5
     basePlateThickness = 0.5
+    baseColors = np.array([[0.5, 0.3, 0.2, 1] for i in range(12)])
+
+    # all verts for shapes
     baseStandVerts = np.array([[basePlateLength, 0, 0],  # 0
                                [0, 0, 0],  # 1
                                [0, basePlateLength, 0],  # 2
@@ -39,7 +39,7 @@ def init_arm(w):
                                [basePlateLength, 0, -basePlateThickness]])  # 7
 
     # Create same stands
-    basePlate = gl.GLMeshItem(vertexes=baseStandVerts, faces=renderFaces, faceColors=renderColors,
+    basePlate = gl.GLMeshItem(vertexes=baseStandVerts, faces=renderFaces, faceColors=baseColors,
                               drawEdges=True, edgeColor=(0, 0, 0, 1))
 
     # Move stands to correct coord
@@ -50,7 +50,7 @@ def init_arm(w):
     w.addItem(basePlate)
 
     # --- Create Base Stands ---
-    heightBaseStand = 12
+    heightBaseStand = 12.6
     widthBaseStand = 3.5
     baseStandVerts = np.array([[widthBaseStand, 0, 0],  # 0
                                [0, 0, 0],  # 1
@@ -62,9 +62,9 @@ def init_arm(w):
                                [widthBaseStand, 0, heightBaseStand]])  # 7
 
     # Create same stands
-    baseStand1 = gl.GLMeshItem(vertexes=baseStandVerts, faces=renderFaces, faceColors=renderColors,
+    baseStand1 = gl.GLMeshItem(vertexes=baseStandVerts, faces=renderFaces, faceColors=baseColors,
                                drawEdges=True, edgeColor=(0, 0, 0, 1))
-    baseStand2 = gl.GLMeshItem(vertexes=baseStandVerts, faces=renderFaces, faceColors=renderColors,
+    baseStand2 = gl.GLMeshItem(vertexes=baseStandVerts, faces=renderFaces, faceColors=baseColors,
                                drawEdges=True, edgeColor=(0, 0, 0, 1))
 
     # Translate stands
@@ -79,27 +79,80 @@ def init_arm(w):
     w.addItem(baseStand2)
 
     # --- Create Arm One ---
-    arm1Length = 20  # in inches
+    arm1Length = 22  # in inches
     arm1Width = 1
-    arm1Verts = np.array([[basePlateLength, 0, 0],  # 0
+    arm1Height = 11.57
+    arm1Colors = np.array([[1, 0, 1, 1] for i in range(12)])
+
+    arm1Verts = np.array([[arm1Width, 0, 0],  # 0
                           [0, 0, 0],  # 1
-                          [0, basePlateLength, 0],  # 2
-                          [0, 0, -basePlateThickness],  # 3
-                          [basePlateLength, basePlateLength, 0],  # 4
-                          [basePlateLength, basePlateLength, -basePlateThickness],  # 5
-                          [0, basePlateLength, -basePlateThickness],  # 6
-                          [basePlateLength, 0, -basePlateThickness]])  # 7
+                          [0, arm1Width, 0],  # 2
+                          [0, 0, arm1Length],  # 3
+                          [arm1Width, arm1Width, 0],  # 4
+                          [arm1Width, arm1Width, arm1Length],  # 5
+                          [0, arm1Width, arm1Length],  # 6
+                          [arm1Width, 0, arm1Length]])  # 7
 
     # Create same stands
-    basePlate = gl.GLMeshItem(vertexes=baseStandVerts, faces=renderFaces, faceColors=renderColors,
-                              drawEdges=True, edgeColor=(0, 0, 0, 1))
+    arm1 = gl.GLMeshItem(vertexes=arm1Verts, faces=renderFaces, faceColors=arm1Colors,
+                         drawEdges=True, edgeColor=(0, 0, 0, 1))
 
     # Move stands to correct coord
-    basePlate.translate(baseDistFromTable - basePlateLength,
-                        (ytable / 2) - (basePlateLength / 2),
-                        0)
+    arm1.translate(baseDistFromTable - (basePlateLength / 2) - (arm1Width / 2),
+                   (ytable / 2) - (arm1Width / 2),
+                   arm1Height)
 
-    w.addItem(basePlate)
+    w.addItem(arm1)
+
+    # --- Create Arm Two ---
+    arm2Length = 22  # in inches
+    arm2Width = 1
+    arm2Colors = np.array([[1, 1, 0, 1] for i in range(12)])
+
+    arm2Verts = np.array([[arm2Width, 0, 0],  # 0
+                          [0, 0, 0],  # 1
+                          [0, arm2Width, 0],  # 2
+                          [0, 0, arm2Length],  # 3
+                          [arm2Width, arm2Width, 0],  # 4
+                          [arm2Width, arm2Width, arm2Length],  # 5
+                          [0, arm2Width, arm2Length],  # 6
+                          [arm2Width, 0, arm2Length]])  # 7
+
+    # Create same stands
+    arm2 = gl.GLMeshItem(vertexes=arm2Verts, faces=renderFaces, faceColors=arm2Colors,
+                         drawEdges=True, edgeColor=(0, 0, 0, 1))
+
+    # Move stands to correct coord
+    arm2.translate(baseDistFromTable - (basePlateLength / 2) - (arm1Width / 2),
+                   (ytable / 2) - (arm1Width / 2),
+                   32.5)
+
+    w.addItem(arm2)
+
+    # --- Create End Joint ---
+    arm3Length = 3.25  # meant to be rotated and flapped
+    arm3Width = 0.5
+    arm3Colors = np.array([[1, 0, 0.6, 1] for i in range(12)])
+
+    arm3Verts = np.array([[arm3Width, 0, 0],  # 0
+                          [0, 0, 0],  # 1
+                          [0, arm3Width, 0],  # 2
+                          [0, 0, arm3Length],  # 3
+                          [arm3Width, arm3Width, 0],  # 4
+                          [arm3Width, arm3Width, arm3Length],  # 5
+                          [0, arm3Width, arm3Length],  # 6
+                          [arm3Width, 0, arm3Length]])  # 7
+
+    # Create same stands
+    arm3 = gl.GLMeshItem(vertexes=arm3Verts, faces=renderFaces, faceColors=arm3Colors,
+                         drawEdges=True, edgeColor=(0, 0, 0, 1))
+
+    # Move stands to correct coord
+    arm3.translate(baseDistFromTable - (basePlateLength / 2) - (arm3Width / 2),
+                   (ytable / 2) - (arm3Width / 2),
+                   54)
+
+    w.addItem(arm3)
 
     return baseStand1
 
