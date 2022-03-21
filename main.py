6d19280db9e82053aba3720b20793d668b8e866a
Chris Palazzo <61244661+ChrisPal323@users.noreply.github.com>
2022-03-21 09:01:02 -0400

    added side stands

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import math
-import random
 import sys
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import cv2
-from collections import deque
 import pyqtgraph.opengl as gl
-from pyqtgraph.Qt import QtGui
+from pyqtgraph.Qt import QtWidgets
 
 # Numbers that work well (in inches) with the graph and keep proper proportions
 xtable = 108
@@ -18,40 +14,65 @@ ynet = 6  # Kinda like a z coord after 90 degree rotation
 
 # Init robot arm in frame
 def init_arm(w):
+    # Init Cube Faces for all
+    renderFaces = np.array([[1, 0, 7], [1, 3, 7],
+                            [1, 2, 4], [1, 0, 4],
+                            [1, 2, 6], [1, 3, 6],
+                            [0, 4, 5], [0, 7, 5],
+                            [2, 4, 5], [2, 6, 5],
+                            [3, 6, 5], [3, 7, 5]])
+
+    # Render colors for all
+    renderColors = np.array([[1, 0, 1, 1] for i in range(12)])
+
     # -- Create Arm Base --
-    basePlate = 12  # in inches
-    armBasePlate = gl.GLGridItem()  # Create base
-    armBasePlate.setColor((255, 0, 255, 255))
-    armBasePlate.translate(-10.5 - (basePlate / 2), (ytable / 2), 0)  # Move to correct coord
-    armBasePlate.setSize(basePlate, basePlate)  # Size table
-    w.addItem(armBasePlate)
-
-    # -- Create Base Stands --
-    baseStandVerts = np.array([[1, 0, 0],  # 0
-                                [0, 0, 0],  # 1
-                                [0, 1, 0],  # 2
-                                [0, 0, 1],  # 3
-                                [1, 1, 0],  # 4
-                                [1, 1, 1],  # 5
-                                [0, 1, 1],  # 6
-                                [1, 0, 1]])  # 7
-
-    baseStandFaces = np.array([[1, 0, 7], [1, 3, 7],
-                                [1, 2, 4], [1, 0, 4],
-                                [1, 2, 6], [1, 3, 6],
-                                [0, 4, 5], [0, 7, 5],
-                                [2, 4, 5], [2, 6, 5],
-                                [3, 6, 5], [3, 7, 5]])
-
-    colors = np.array([[1, 0, 0, 1] for i in range(12)])
+    basePlateLength = 12  # in inches
+    baseDistFromTable = -10.5
+    basePlateThickness = 0.5
+    baseStandVerts = np.array([[basePlateLength, 0, 0],  # 0
+                               [0, 0, 0],  # 1
+                               [0, basePlateLength, 0],  # 2
+                               [0, 0, -basePlateThickness],  # 3
+                               [basePlateLength, basePlateLength, 0],  # 4
+                               [basePlateLength, basePlateLength, -basePlateThickness],  # 5
+                               [0, basePlateLength, -basePlateThickness],  # 6
+                               [basePlateLength, 0, -basePlateThickness]])  # 7
 
     # Create same stands
-    baseStand1 = gl.GLMeshItem(vertexes=baseStandVerts, faces=baseStandFaces, faceColors=colors,
+    basePlate = gl.GLMeshItem(vertexes=baseStandVerts, faces=renderFaces, faceColors=renderColors,
+                              drawEdges=True, edgeColor=(0, 0, 0, 1))
+
+    # Move stands to correct coord
+    basePlate.translate(baseDistFromTable - basePlateLength,
+                        (ytable / 2) - (basePlateLength / 2),
+                        0)
+
+    w.addItem(basePlate)
+
+    # -- Create Base Stands --
+    heightBaseStand = 12
+    widthBaseStand = 4
+    baseStandVerts = np.array([[widthBaseStand, 0, 0],  # 0
+                               [0, 0, 0],  # 1
+                               [0, widthBaseStand, 0],  # 2
+                               [0, 0, heightBaseStand],  # 3
+                               [widthBaseStand, widthBaseStand, 0],  # 4
+                               [widthBaseStand, widthBaseStand, heightBaseStand],  # 5
+                               [0, widthBaseStand, heightBaseStand],  # 6
+                               [widthBaseStand, 0, heightBaseStand]])  # 7
+
+    # Create same stands
+    baseStand1 = gl.GLMeshItem(vertexes=baseStandVerts, faces=renderFaces, faceColors=renderColors,
                                drawEdges=True, edgeColor=(0, 0, 0, 1))
-    baseStand2 = gl.GLMeshItem(vertexes=baseStandVerts, faces=baseStandFaces, faceColors=colors,
+    baseStand2 = gl.GLMeshItem(vertexes=baseStandVerts, faces=renderFaces, faceColors=renderColors,
                                drawEdges=True, edgeColor=(0, 0, 0, 1))
 
     # Translate stands
+    baseStand1.translate(baseDistFromTable - (widthBaseStand / 2) - (basePlateLength / 2),
+                         (ytable / 2) + (basePlateLength / 2) - widthBaseStand,
+                         0)
+    baseStand2.translate(baseDistFromTable - (widthBaseStand / 2) - (basePlateLength / 2),
+                         (ytable / 2) - widthBaseStand, 0)
 
     # Add to frame
     w.addItem(baseStand1)
@@ -63,7 +84,7 @@ def init_arm(w):
 # Main Testing Loop
 def main():
     # ---- Set Up Window ----
-    app = QtGui.QApplication(sys.argv)  # Truthfully, don't know why we need this
+    app = QtWidgets.QApplication(sys.argv)  # Truthfully, don't know why we need this
     w = gl.GLViewWidget()
     w.opts['distance'] = 150
     w.setWindowTitle('Ping-Pong Robot-Control')
